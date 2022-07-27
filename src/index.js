d8ece9a82e2c47ade2cc555dadb1d541e384a66b
k8 <make.me.loco@gmail.com>
2022-07-27 20:59:41 +0300

    feat: add brackets function.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = [];
+  bracketsConfig.forEach((val) => {
+    brackets.push(val[0] + val[1]);
+  });
+
+  let myStr = str;
+
+  while (myStr) {
+    let tmpStr = myStr;
+    brackets.forEach(function (item) {
+      myStr = myStr.replace(item, '');
+    });
+    if (tmpStr === myStr) {
+      return false;
+    }
+  }
+  if (myStr === '') {
+    return true;
+  }
 }
